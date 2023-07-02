dac63247e636cdcf5e1ae4f2c1e19b9644063355
piolad <piolad2003@gmail.com>
2023-07-02 13:28:38 +0200

    test the download process

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,74 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        if(args.length != 0){
+            //do stuff with args
+        }
+        else{
+            String urltxt = "https://gosafir.com/mag/wp-content/uploads/2019/12/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.pdf";
+            URL url = new URL(urltxt);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+
+
+
+
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("HEAD");
+
+            long removeFileSize = httpConnection.getContentLengthLong();
+
+            System.out.println("File size: " + removeFileSize);
+
+            String fieldValue = httpConnection.getHeaderField("Content-Disposition");
+            if(fieldValue == null || !fieldValue.contains("filename=\"")){
+
+                System.out.println("No file name");
+                System.out.println("fieldValue: " + fieldValue);
+                System.out.println("substrd: " + fieldValue.substring(fieldValue.indexOf("filename=\"") + 10, fieldValue.length() ));
+                return;
+            }
+            String fileName = fieldValue.substring(fieldValue.indexOf("filename=\"") + 10, fieldValue.length() - 1);
+
+            System.out.println("File name: " + fileName);
+
+
+
+            File file = new File("", fileName);
+            if(file.exists()){
+                System.out.println("File exists");
+                System.out.println("File length: " + file.length());
+            }
+
+
+            if(file.length() < removeFileSize) {
+                System.out.println("File is not complete");
+
+                OutputStream os = new FileOutputStream("a.png", true);
+
+                BufferedInputStream in = new BufferedInputStream(url.openStream());
+                byte dataBuffer[] = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    os.write(dataBuffer, 0, bytesRead);
+                }
+
+
+                in.close();
+            }
+
+
+
+
+            System.out.println("File size: " + removeFileSize);
+            System.out.println("File name: " + fileName);
+
         }
     }
 }
